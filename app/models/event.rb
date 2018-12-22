4cdf90bf4f5f0826bc041099099a27449dc96458
Ryan Chase <rchase@medely.com>
2018-12-22 14:05:26 -0800

    getting started but can't update model fields

---
+++ app/models/event.rb
@@ -0,0 +1,3 @@
+class Event < ActiveRecord::Base
+  attr_accessor :tag, :description
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :events
+  root to: "home#index"
+
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
